039fff42b9babb9b5a37b89052d131dfabe8f507
Perkles <caioln.077@gmail.com>
2023-06-04 16:22:18 -0300

    content created

---
+++ src/app/Data/dataFake.ts
@@ -0,0 +1,10 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:
+      'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/06/homem_aranha_atraves_do_aranhaverso_referencias__ktix301m-1210x544.jpg',
+    title: 'Beyond The Spider-Verse',
+    description:
+      'Continuação de Homem-Aranha já é confirmada para 2024 após final que deixou fans com um gosto do que acontecerá no próximo, já que o final é completamente aberto para sua continuação.',
+  },
+];
