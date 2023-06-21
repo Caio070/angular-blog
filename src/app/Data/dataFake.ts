eccffa4b852cdb4c3944b930e04a28e58a19e4e6
Perkles <caioln.077@gmail.com>
2023-06-21 17:41:28 -0300

    finalization of details

--- src/app/Data/dataFake.ts
+++ src/app/Data/dataFake.ts
@@ -7,4 +7,29 @@ export const dataFake = [
     description:
       'Continuação de Homem-Aranha já é confirmada para 2024 após final que deixou fans com um gosto do que acontecerá no próximo, já que o final é completamente aberto para sua continuação.',
   },
+  {
+    id: '2.1',
+    photoCover:
+      'https://ultraverso.com.br/wp-content/uploads/2023/04/The-Flash-Trailer.png',
+    title: 'The flash se aproxima de sua estreia nos cinemas.',
+    description:
+      'The flash promete adaptar o Flash Point e reinciar o universo da DC nos cinemas.',
+  },
+  {
+    id: '2.2',
+    photoCover: 'https://i.ytimg.com/vi/dEg5PGwWxso/maxresdefault.jpg',
+    title:
+      'Velozes e Furiosos 10 faz sua estreia e deixa final aberto para sua segunda parte.',
+    description:
+      'Franquia ainda terá mais 2 filmes comoparte de ser encerramento. VeF 10 termina com final aberto já para próximo filme que será lançado no ano seguinte.',
+  },
+  {
+    id: '2.3',
+    photoCover:
+      'https://classic.exame.com/wp-content/uploads/2023/05/Black-Mirror-6-Trailer-da-sexta-temporada-da-serie-distopica-de-ficcao-cientifica-da-Netflix.jpg?quality=70&strip=info&w=1024',
+    title:
+      'Sexta temporada de Black Mirror é confirmada e terá participação de Aaron Paul.',
+    description:
+      'Beyond the Sea se passa em um 1969 alternativo, onde dois homens em uma perigosa missão com alta tecnologia lutam com as consequências de uma tragédia inimaginável. Dirigido por John Crowley, é estrelado por Aaron Paul, Josh Hartnett, Kate Mara, Auden Thornton e Rory Culkin. Dado o traje espacial visível na foto abaixo, vale lembrar que 1969 foi o ano em que os Estados Unidos enviaram um homem à Lua”, diz a sinopse do episódio da 6ª temporada em que Aaron Paul aparece.',
+  },
 ];
